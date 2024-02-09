bc656e3bd6714b1b8338cec44fd59eb4f1003b20
Alexander Sosnovsky <106810534+LeshenShow@users.noreply.github.com>
2024-02-09 16:36:25 +0300

    83 reselect нужно потом аддаптировать под hook

--- src/redux/usersSelectors.js
+++ src/redux/usersSelectors.js
@@ -1,7 +1,18 @@
+import { createSelectorHook } from "react-redux";
+
+// PRIMITIVE SELECTORS
 export const getUsers = (state) => state.usersPage.users;
+// код ниже будет создавать новый массив и будет постоянно срабатывать перерисовка в mapstatetoprops
+//export const getUsers = (state) => state.usersPage.users.filter(u=>true)
 export const getPageSize = (state) => state.usersPage.pageSize;
 export const getTotalUsersCount = (state) => state.usersPage.totalUsersCount;
 export const getCurrentPage = (state) => state.usersPage.currentPage;
 export const getIsFetching = (state) => state.usersPage.isFetching;
 export const getFollowingInProgress = (state) =>
   state.usersPage.followingInProgress;
+//
+// надо разобраться как работает через хуки
+// const getUsersSelector = (state) => state.usersPage.users;
+// export const getUsers = createSelectorHook((getUsersSelector, users) => {
+//   users.filter((users) => true);
+// });
